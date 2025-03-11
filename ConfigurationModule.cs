441c3ca1a79d30514f35b81847c14e92893a00c6
HP <HP@DESKTOP-T6A34MC>
2025-03-12 00:05:56 +0500

    (При последующей разработке возможны изменения в проекте) Добавлен пакет NuGet - Newtonsoft.Json. Создан модуль конфигурации. Бугрина А.Е.

---
+++ ConfigurationModule.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+using Newtonsoft.Json;
+
+namespace TCPDataValidator
+{
+    public class ConfigurationModule
+    {
+        public class ServerConfig
+        {
+            public string IpAddress { get; set; }
+            public int Port { get; set; }
+        }
+        public class AppConfig
+        {
+            public ServerConfig[] Servers { get; set; }
+            public int ResultServerPort { get; set; }
+        }
+        public AppConfig LoadConfig(string configFilePath)
+        {
+            if (!File.Exists(configFilePath))
+            {
+                throw new FileNotFoundException("Файл конфигурации не найден.");
+            }
+
+            string json = File.ReadAllText(configFilePath);
+            return JsonConvert.DeserializeObject<AppConfig>(json);
+        }
+        public void SaveConfig(AppConfig config, string configFilePath)
+        {
+            string json = JsonConvert.SerializeObject(config, Formatting.Indented);
+            File.WriteAllText(configFilePath, json);
+        }
+    }
+}
