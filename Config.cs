62f5ee342f7ab98930292d2cc7762f6999ecccbf
HP <HP@DESKTOP-T6A34MC>
2025-03-19 18:44:45 +0500

    Добавлен файл json для хранения данных. Класс конфигурации изменён - Config. Создан обработчик пакетов - PacketProcessor. Бугрина А.Е.

---
+++ Config.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+using Newtonsoft.Json;
+
+namespace TCPDataValidator
+{
+    public class Config
+    {
+        public class ServerConfig // Вложенный класс для описания одного сервера
+        {
+            public string IpAddress { get; set; }
+            public int Port { get; set; }
+        }
+        public ServerConfig[] Servers { get; set; }
+        public int ResultServerPort { get; set; }
+        public static Config LoadConfig(string path)
+        {
+            try
+            {
+                string json = File.ReadAllText(path);
+                return JsonConvert.DeserializeObject<Config>(json); // Десериализация JSON в объект Config
+            }
+            catch (Exception ex)
+            {
+                throw new ApplicationException($"Ошибка загрузки конфигурации: {ex.Message}", ex);
+            }
+        }
+    }
+}
---
+++ PacketProcessor.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace TCPDataValidator
+{
+    public static class PacketProcessor
+    {
+        public static (string Data1, string Data2) ProcessPacket(string packet) // Метод для обработки строки данных
+        {
+            var match = Regex.Match(packet, @"#90#010102#27(\d{6}|\d{8});(\d{6}|\d{8})#91");
+            if (match.Success)
+            {
+                return (match.Groups[1].Value, match.Groups[2].Value);
+            }
+            throw new FormatException("Неверный формат пакета");
+        }
+    }
+}
